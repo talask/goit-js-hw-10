39e40dd1b1f878829ca3bd4649a28199baad1095
talask <natasha169@ukr.net>
2023-04-23 01:46:22 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -3,26 +3,66 @@ import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 
 
-import fetchCountries from './js/fetchCountries';
+import {fetchCountries} from './js/fetchCountries';
 import Notiflix from 'notiflix';
 let  debounce  = require('lodash.debounce'); 
 
 const inputSearch = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const div = document.querySelector('.country-info');
- console.log("script");
+ 
 inputSearch.addEventListener('input', debounce(() => {
-  console.log("listener");
-  const value = trim(inputSearch);
+ 
+  const value = inputSearch.value.trim();
   if(value.length) {
+
   return fetchCountries(value)
     .then(data => {
-    // Data handling
-      console.log(data);
+    
+      div.innerHTML = '';
+      list.innerHTML = '';
+     
+      if(data.length == 1){
+        
+        div.innerHTML = createFindCard(data[0]);
+
+      }else if(data.length <= 10) {
+        
+        list.insertAdjacentHTML("beforeend", createFindList(data));
+
+      } else {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      }
     })
     .catch(error => {
-    // Error handling
-    Notiflix.Notify.failure('Oops, there is no country with that name');
+    
+      div.innerHTML = '';
+      list.innerHTML = '';
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+   
   });
 }
-}, 300));
+}, DEBOUNCE_DELAY));
+
+function createFindList(arr) {
+ 
+  return arr.map(({name: { official } ,flags: { svg, alt }}) => {
+    return `
+    <li>
+      <img src='${svg}' alt='${alt}' width='24'>
+      ${official}
+    </li>
+    `}).join('');
+
+}
+
+function createFindCard({name: { official }, flags: { svg, alt }, capital,population, languages}) {
+ 
+  return `
+    <h2><img src='${svg}' alt='${alt}' width='34'>
+    ${official}</h2>
+    <p><span>Capital:</span> ${[...capital]}</p>
+    <p><span>Languages:</span> ${Object.values(languages).join(', ')}</p>
+    <p><span>Population:</span> ${population}</p>
+  `
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,9 +4,11 @@ export function fetchCountries(name) {
  
   return fetch(`${BASE_URL}/${name}?${PARAM}`)
     .then(response => {
-    // Response handling
+    
       if(!response.ok) {
+
         throw new Error(response.status);
+        
       }
       return response.json();
     });
